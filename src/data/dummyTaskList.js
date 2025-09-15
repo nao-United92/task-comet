e4d31ed449546feee601ce613caff067e83e1c1b
nao-United92 <naoyauver@icloud.com>
2025-09-16 00:12:50 +0900

    feat: Add TaskItem component and dummy task list

---
+++ src/data/dummyTaskList.js
@@ -0,0 +1,17 @@
+export const dummyTaskList = [
+  {
+    id: 1,
+    title: "Reactを勉強する",
+    status: "notStarted",
+  },
+  {
+    id: 2,
+    title: "JavaScriptを勉強する",
+    status: "inProgress",
+  },
+  {
+    id: 3,
+    title: "TypeScriptを勉強する",
+    status: "completed",
+  },
+];
