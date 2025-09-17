0172ffc9c6a21d73f578625f42d7ba07de762c25
nao-United92 <naoyauver@icloud.com>
2025-09-18 00:19:54 +0900

    feat: Add create task form

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,17 +1,37 @@
-import { dummyTaskList } from "../data/dummyTaskList";
-import { TaskItem } from "./TaskItem";
+import { useState } from "react";
+import { CreateTaskForm } from "./CreateTaskForm";
+import { TaskItem } from "./Taskitem";
 
 export function TaskList() {
+  // タスク一覧の状態を管理
+  const [taskList, serTaskList] = useState([]);
+
+  // 新しいタスクを追加
+  const createTask = (title) => {
+    setTaskList((prevTaskList) => {
+      // 新しいタスクオブジェクトクォ生成
+      const newTask = {
+        id: Date.now(), // 現在の時刻を ID として使用
+        title,
+        status: "notStarted", // 初期状態は未着手
+      };
+      // 既存のタスクに新しいタスクを追加
+      return [...prevTaskList, newTask];
+    });
+  };
+
   return (
-    <div className="space-y-3 px-10 pb-10">
+    <div className="relative">
+      <div className="sticky top-0 flex flex-col items-end gap-2 bg-slate-100 px-10 py-5">
+        <div className="w-full">
+          <CreateTaskForm onSubmit={createTask} />
+        </div>
+      </div>
       <div className="space-y-3 px-10 pb-10">
-        {dummyTaskList.length === 0 ? (
-          // タスクが0件の場合はフォールバックを表示
+        {taskList.length === 0 ? (
           <p className="text-center text-sm">タスクがありません</p>
         ) : (
-          dummyTaskList.map((task) => (
-            <TaskItem key={task.id} task={task} /> // 各タスクアイテムを表示
-          ))
+          taskList.map((task) => <TaskItem key={task.id} task={task} />)
         )}
       </div>
     </div>
